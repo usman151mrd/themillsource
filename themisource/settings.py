7f908605c2c25a45aa18a02b505b55c38b65864a
Muhammad Usman <sp18-rcs-027@cuilahore.edu.pk>
2021-06-28 01:06:09 +0500

    Update settings.py

--- themisource/settings.py
+++ themisource/settings.py
@@ -91,7 +91,7 @@ if DEVELOPMENT_MODE is True:
             'NAME': 'themisource',
             'USER': 'themisource',
             'PASSWORD': 'huruhujhyob1z8v3',
-            'HOST': 'app-d4dfbce5-3dc3-4fcc-bad7-0102ca3f0a6a-do-user-8599880-0.b.db.ondigitalocean.com',
+            'HOST': 'localhost',
             'PORT': '25060',
         },
         "sqlite": {
