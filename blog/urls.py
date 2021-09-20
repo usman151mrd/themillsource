30c7d9c0df38e89e595bb9fdaecaeefcbc43d106
Imran Zahoor <imran-zahoor@outlook.com>
2021-09-20 16:52:07 +0500

    advertise page completed

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('about-us/', views.about_us_page, name='about-us'),
     path('write-for-us/', views.write_for_us_page, name='write-for-us'),
     path('send-us-your-pr/', views.send_us_your_pr_page, name='send-us-your-pr'),
+    path('advertise/', views.advertise_page, name='advertise'),
 ]
