d6a5d745dcf32c059d77db6473d1dd2a7a53dfa8
Imran Zahoor <imran-zahoor@outlook.com>
2021-10-05 11:49:46 +0500

    greeting message qoma removed

--- staticfiles/js/GreetingPlugin.js
+++ staticfiles/js/GreetingPlugin.js
@@ -5,19 +5,19 @@
       var text, color;
   
       if (now >= 6 && now < 12){
-        text = 'Good Morning,';
+        text = 'Good Morning';
         color = '#008100';
       }
       else if (now >= 12 && now < 17){
-        text = 'Hello,';
+        text = 'Hello';
         color = '#CC7A29';  
       }
       else if (now >= 17 && now < 22 ){
-        text = 'Good Evening,';
+        text = 'Good Evening';
         color = '#005CE6';
       }
       else{
-        text = 'Good Night,';
+        text = 'Good Night';
         color = '#001C53';
       }
   
