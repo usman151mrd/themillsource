4091e6b10d83e75f514f6e8d14b919f54add26c5
Imran Zahoor <imran-zahoor@outlook.com>
2021-10-14 20:07:28 +0500

    theme switcher added

--- static/js/script.js
+++ static/js/script.js
@@ -11,5 +11,35 @@ $(document).ready(function(){
 
     $('#file-btn').click(function(){
         $('#pr-image').click()
-    })
+    })   
+    
+    initTheme();
 });
+
+ // function to set a given theme/color-scheme
+ function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+        document.querySelector("#site-logo").src="/static/images/logo_dark.png";
+    } else {
+        setTheme('theme-dark');
+        document.querySelector("#site-logo").src="/static/images/logo_light.png";
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+function initTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+        document.querySelector("#site-logo").src="/static/images/logo_light.png";
+    } else {
+        setTheme('theme-light');
+        document.querySelector("#site-logo").src="/static/images/logo_dark.png";
+    }
+};
